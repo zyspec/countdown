b3c90c813cb7cadcbe1efc88121aa130425a3d26
zyspec <zyspec@yahoo.com>
2020-04-30 15:38:55 -0500

    update language files

--- language/english/main.php
+++ language/english/main.php
@@ -1,9 +1,9 @@
 <?php
 define('_CD_TEXT_COUNTDOWNS', 'Countdown Events');
-define('_CD_TEXT_NAME', 'Event Name:');
-define('_CD_TEXT_DESCRIPTION', 'Description:');
-define('_CD_TEXT_EXPIRATION', 'Expiration Date/Time:');
-define('_CD_TEXT_REMAINING', 'Remaining Time:');
+define('_CD_TEXT_NAME', 'Event Name');
+define('_CD_TEXT_DESCRIPTION', 'Description');
+define('_CD_TEXT_EXPIRATION', 'Expiration Date/Time');
+define('_CD_TEXT_REMAINING', 'Remaining Time');
 define('_CD_TEXT_ADD_EVENT', 'Add Countdown');
 define('_CD_TEXT_UPDATE_EVENT', 'Update Countdown');
 define('_CD_TEXT_EXPIRATION_TIME', 'Expiration Time');
@@ -17,7 +17,7 @@ define('_CD_TEXT_DATE_HHMMSS', '(HH:MM:SS)');
 define('_CD_TEXT_ADD_COUNTDOWN', 'Add');
 define('_CD_TEXT_CURRENTTIME', 'Current Time:');
 
-//Time Constants
+// Time Constants
 define('_COUNTDOWN_TIME_YEAR', 'year');
 define('_COUNTDOWN_TIME_YEARS', 'years');
 define('_COUNTDOWN_TIME_WEEK', 'week');
@@ -30,3 +30,19 @@ define('_COUNTDOWN_TIME_MIN', 'minute');
 define('_COUNTDOWN_TIME_MINS', 'minutes');
 define('_COUNTDOWN_TIME_SEC', 'second');
 define('_COUNTDOWN_TIME_SECS', 'seconds');
+define('_COUNTDOWN_TIME_AM', 'AM');
+define('_COUNTDOWN_TIME_PM', 'PM');
+
+// User Messages
+define('_COUNTDOWN_MSG_UPDATED', 'Countdown updated successfully!');
+define('_COUNTDOWN_MSG_CREATED', 'Countdown created successfully!');
+define('_COUNTDOWN_MSG_DELETED', 'Countdown removed successfully!');
+define('_COUNTDOWN_MSG_EXPIRED', 'Expired');
+define('_COUNTDOWN_MSG_NO_EVENTS', 'There aren\'t any events to show');
+define('_COUNTDOWN_MSG_DEL_CONFIRM', 'Are you sure you want to delete the \'%s\' countdown?');
+define('_COUNTDOWN_ERR_SAVE', 'Errors while saving countdown.');
+define('_COUNTDOWN_ERR_DEL', 'Errors while removing countdown.');
+define('_COUNTDOWN_ERR_BAD_FORMAT', 'Date format is incorrect.');
+define('_COUNTDOWN_ERR_BAD_ID', 'Invalid countdown, please try again.');
+define('_COUNTDOWN_ERR_NOT_OWNER', 'You do not own this countdown.');
+
--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -1,11 +1,11 @@
 <?php
-define('_MI_COUNTDOWN_NAME', 'countdown');
+define('_MI_COUNTDOWN_NAME', 'Countdown');
 define('_MI_COUNTDOWN_DESC', 'Counts down time for user created events.');
 define('_MI_CD_TEMP_COUNTDOWN', 'Template for index.php.');
 define('_MI_CD_TEMP_COUNTDOWN_ADD', 'Template used for adding new countdowns.');
 define('_MI_CD_TEMP_COUNTDOWN_EDIT', 'Template used for editing existing countdowns.');
 
-define('_MI_MENU_ADD_NEW', 'Submit');
+define('_MI_MENU_ADD_NEW', 'Create');
 define('_MI_MENU_VIEW_CURRENT', 'View Current');
 define('_MI_MENU_VIEW_ALL', 'View All');
 define('_MI_MENU_VIEW_EXPIRED', 'View Expired');
